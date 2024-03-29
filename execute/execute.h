b7f8902d45f66c6ca51520f70a537b1451257d25
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-03-29 20:03:55 +0900

    Style: norminette

--- execute/execute.h
+++ execute/execute.h
@@ -6,22 +6,18 @@
 /*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:56:57 by yeoshin           #+#    #+#             */
-/*   Updated: 2024/03/29 19:14:29 by soljeong         ###   ########.fr       */
+/*   Updated: 2024/03/29 20:02:33 by soljeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
-
 # define FALSE -1
 # define TRUE 1
-#include "../minishell.h"
-
+# include "../minishell.h"
 
 void	free_list(t_list *head);
 void	ft_exit(t_list *head, t_env *env_list);
 void	error_handler(char *cmd_name, char *option_name, char *msg);
 int		execute(t_list *head, t_env *env_list);
-
-
 #endif
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 19:05:52 by soljeong          #+#    #+#             */
-/*   Updated: 2024/03/29 19:14:12 by soljeong         ###   ########.fr       */
+/*   Updated: 2024/03/29 20:03:10 by soljeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <stdlib.h>
 # include <sys/errno.h>
 # include <stdlib.h>
-#include "execute/execute.h"
-#include "libft/libft.h"
-#include "parse/parse_list.h"
-#include "parse/parse_env.h"
-#include "printf/ft_printf.h"
-# endif
+# include "execute/execute.h"
+# include "libft/libft.h"
+# include "parse/parse_list.h"
+# include "parse/parse_env.h"
+# include "printf/ft_printf.h"
+#endif
