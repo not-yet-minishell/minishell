bf3c06c4d2c51d82ffd514ef145de9e0a6725822
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-03-29 19:09:31 +0900

    Style: included all headers in minshell.h

---
+++ minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 19:05:52 by soljeong          #+#    #+#             */
+/*   Updated: 2024/03/29 19:07:36 by soljeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "execute/execute.h"
+#include "libft/libft.h"
+#include "parse/parse_list.h"
+#include "printf/ft_printf.h"
+# endif
