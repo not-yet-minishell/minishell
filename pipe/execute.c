2c8eb09b326c5cff0e573f3b9120046ba32d9917
Yeonggi Shin <yeoshin@c2r4s4.42seoul.kr>
2024-03-29 20:55:06 +0900

    Feat: pipe is half implemented

--- pipe/execute.c
+++ pipe/execute.c
@@ -1,4 +1,4 @@
-e/* ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
@@ -6,7 +6,39 @@ e/* ************************************************************************** *
 /*   By: yeoshin <yeoshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 19:59:34 by yeoshin           #+#    #+#             */
-/*   Updated: 2024/03/29 19:59:35 by yeoshin          ###   ########.fr       */
+/*   Updated: 2024/03/29 20:49:49 by yeoshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+void	execute(t_list *node)
+{
+	char	**exe;
+
+	exe = make_list_to_array(node);
+
+}
+
+char	**make_list_to_array(t_list *node)
+{
+	char	**ret;
+	int		list_len;
+	t_list	*pre;
+	int		idx;
+
+	idx = 0;
+	list_len = ft_lstsize(node);
+	ret = (char **)ft_malloc(sizeof(char *) * (list_len + 1));
+	while (node != NULL)
+	{
+		ret[idx] = node->content;
+		pre = node;
+		node = node->next;
+		free(pre->content);
+		free(pre);
+		idx++;
+	}
+	ret[idx] = NULL;
+	return (ret);
+}
--- pipe/redirect.c
+++ pipe/redirect.c
@@ -6,7 +6,37 @@
 /*   By: yeoshin <yeoshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 19:59:20 by yeoshin           #+#    #+#             */
-/*   Updated: 2024/03/29 19:59:25 by yeoshin          ###   ########.fr       */
+/*   Updated: 2024/03/29 20:41:10 by yeoshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+void	in_redirect(t_rd_node *node);
+void	out_redirect(t_rd_node *node);
+void	append_redirect(t_rd_node *node);
+void	heredoc(t_rd_node *node);
+
+void	redirect(t_rd_node *node)
+{
+	enum t_rd_type	type;
+
+	if (type == RD_IN)
+		in_redirect(node);
+	else if(type == RD_HEREDOC)
+		heredoc(node);
+	else if(type == RD_OUT)
+		out_redirect(node);
+	else if(type == RD_APPEND)
+		append_redirect(node);
+}
+
+void	in_redirect(t_rd_node *node)
+{}
+void	out_redirect(t_rd_node *node)
+{}
+void	append_redirect(t_rd_node *node)
+{}
+
+void	heredoc(t_rd_node *node);
+{}
