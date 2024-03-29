d13f599224d06ea94521af806fa3a34118a2a918
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-03-29 19:10:27 +0900

    Style: add parse_env.h in the minishell.h

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 19:05:52 by soljeong          #+#    #+#             */
-/*   Updated: 2024/03/29 19:07:36 by soljeong         ###   ########.fr       */
+/*   Updated: 2024/03/29 19:09:59 by soljeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 #include "execute/execute.h"
 #include "libft/libft.h"
 #include "parse/parse_list.h"
+#include "parse/env.h"
 #include "printf/ft_printf.h"
 # endif
