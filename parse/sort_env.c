7f552ba20d41e4323bd797010bbdd1a04dc2db7e
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-04-08 10:38:41 +0900

    Feat: check normentte sort_env.c

--- parse/sort_env.c
+++ parse/sort_env.c
@@ -6,15 +6,18 @@
 /*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 18:29:18 by soljeong          #+#    #+#             */
-/*   Updated: 2024/04/05 11:52:11 by soljeong         ###   ########.fr       */
+/*   Updated: 2024/04/08 10:37:31 by soljeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_list	*list_delete(t_list *prev, t_list *curr);
-void	print_env(t_list *env_list);
-void select_sort_selectsmall(t_list **head, t_list **new_list);
+t_list		*list_delete(t_list *prev, t_list *curr);
+void		print_env(t_list *env_list);
+void		select_sort_selectsmall(t_list **head, t_list **new_list);
+static void	list_delete_and_add(t_list *small, t_list *small_prev, \
+t_list **new_list, t_list **head);
+
 t_list	*select_sort(t_list *env_list)
 {
 	t_list	*new_list;
@@ -22,21 +25,17 @@ t_list	*select_sort(t_list *env_list)
 
 	head = (env_list)->next;
 	new_list = NULL;
-	while(head)
-	{
-		select_sort_selectsmall(&head,&new_list);
-	}
+	while (head)
+		select_sort_selectsmall(&head, &new_list);
 	return (new_list);
 }
 
-void select_sort_selectsmall(t_list **head, t_list **new_list)
+void	select_sort_selectsmall(t_list **head, t_list **new_list)
 {
-
 	t_list	*curr;
 	t_list	*small_prev;
 	t_list	*small;
 	t_list	*prev;
-	t_list	*temp;
 
 	prev = (*head);
 	curr = (*head)->next;
@@ -44,7 +43,9 @@ void select_sort_selectsmall(t_list **head, t_list **new_list)
 	small_prev = NULL;
 	while (curr)
 	{
-		if (ft_strncmp(((t_env *)small->content)->key,((t_env *)curr->content)->key, ft_strlen(((t_env *)curr->content)->key)) > 0)
+		if (ft_strncmp(((t_env *)small->content)->key, \
+		((t_env *)curr->content)->key, \
+		ft_strlen(((t_env *)curr->content)->key)) > 0)
 		{
 			small = curr;
 			small_prev = prev;
@@ -52,6 +53,14 @@ void select_sort_selectsmall(t_list **head, t_list **new_list)
 		prev = curr;
 		curr = curr->next;
 	}
+	list_delete_and_add(small, small_prev, new_list, head);
+}
+
+static void	list_delete_and_add(t_list *small, t_list *small_prev, \
+t_list **new_list, t_list **head)
+{
+	t_list	*temp;
+
 	if (small == (*head))
 	{
 		temp = (*head)->next;
@@ -60,14 +69,14 @@ void select_sort_selectsmall(t_list **head, t_list **new_list)
 		(*head) = temp;
 	}
 	else
-		ft_lstadd_back(new_list,list_delete(small_prev,small));
+		ft_lstadd_back(new_list, list_delete(small_prev, small));
 }
 
-t_list *list_delete(t_list *prev, t_list *curr)
+t_list	*list_delete(t_list *prev, t_list *curr)
 {
 	if (prev && curr->next)
 		prev->next = curr->next;
-	if (prev && curr&& curr->next == NULL)
+	if (prev && curr && curr->next == NULL)
 		prev->next = NULL;
 	curr->next = NULL;
 	return (curr);
