3db798cad8bcb09012ba2a60110bc12bf4be8271
Yeonggi Shin <yeoshin@c2r3s5.42seoul.kr>
2024-04-25 11:25:00 +0900

    Style:printf, gnl header delete

--- mandatory/minishell.h
+++ mandatory/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soljeong <soljeong@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yeoshin <yeoshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 19:05:52 by soljeong          #+#    #+#             */
-/*   Updated: 2024/04/24 10:28:08 by soljeong         ###   ########.fr       */
+/*   Updated: 2024/04/25 11:24:12 by yeoshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@
 # include "libft/libft.h"
 # include "ast/cmd_tree.h"
 # include "extends/extends.h"
-# include "printf/ft_printf.h"
 # include "parse/parse.h"
 # include "parse/parse_test.h"
 # include "wildcard/wildcard.h"
 # include "pipe/pipex.h"
 # include "execute_builtin/execute_builtin.h"
-# include "get_next_line/get_next_line_bonus.h"
 
 void	error_handler(char *cmd_name, char *option_name, char *msg);
 #endif
